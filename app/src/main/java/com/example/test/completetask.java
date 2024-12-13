c039e622b84193f154bf2ee7c433d41510b59748
nimtharayaddehige <143510507+nimtharayaddehige@users.noreply.github.com>
2024-12-13 15:16:21 +0530

    Updated 24  12 13

--- app/src/main/java/com/example/test/completetask.java
+++ app/src/main/java/com/example/test/completetask.java
@@ -1,6 +1,5 @@
 package com.example.test;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Subscriptionplan extends AppCompatActivity {
+public class completetask extends AppCompatActivity {
 
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_subscriptionplan);
+        setContentView(R.layout.activity_completetask);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/test/taskfeedback.java
+++ app/src/main/java/com/example/test/taskfeedback.java
@@ -1,6 +1,5 @@
 package com.example.test;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Subscriptionplan extends AppCompatActivity {
+public class taskfeedback extends AppCompatActivity {
 
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_subscriptionplan);
+        setContentView(R.layout.activity_taskfeedback);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
